cda0bb1fad159cb64afa210f3d51fc9d5c2e3eed
Divine Sam <DSam+tfg@fabletics.com>
2026-03-02 09:50:06 +0530

    Added sources and folders

---
+++ sql/bronze/bronze_encounters.sql
@@ -0,0 +1,3 @@
+CREATE VIEW bronze.encounters AS 
+SELECT * 
+FROM read_csv_auto('https://docs.google.com/spreadsheets/d/e/2PACX-1vT2uIIwWMIZWMPTp9Z5cBiBO-hlNXPpq8Fz1vG9zuhVYzMYghBEsheZa0Qihz0VORBW--7AOui2l9Rj/pub?gid=604038186&single=true&output=csv', ("header" = CAST('t' AS BOOLEAN)));
